adcf2a920c244972e707b615bfe2924c0944a75f
Tiago Santos <tiagorafaelmelo@gmail.com>
2019-04-16 22:41:32 -0300

    Atualizações.

--- resources/views/produtos/show.blade.php
+++ resources/views/produtos/show.blade.php
@@ -1,13 +1,23 @@
 @extends('layout.app')
 @section('title', $produto->titulo)
 @section('content')
-<h1>{{ $produto->titulo }}</h1>
+    <h1>{{ $produto->titulo }}</h1>
 
-<ul>
-    <li><strong>SKU: </strong> {{ $produto->sku }}</li>
-    <li><strong>Preço: </strong> {{ $produto->preco }}</li>
-    <li><strong>Adicinado em: </strong> {{ date_format($produto->created_at, 'd/m/Y') }}</li>
-</ul>
-<p>{{ $produto->descricao }}</p>
-<a href="javascript:history.go(-1)">Voltar</a>
+    <div class="row">
+        @if(file_exists("./img/produtos/".md5($produto->id).".jpg"))
+        <div class="col-md-6">
+            <img src="{{ url('img/produtos/'.md5($produto->id).".jpg") }}" alt="Imagem Produto" class="img-fluid img-thumbnail">
+        </div>
+        @endif
+        <div class="col-md-6">
+            <ul>
+                <li><strong>SKU: </strong> {{ $produto->sku }}</li>
+                <li><strong>Preço: </strong>R$ {{ number_format($produto->preco, 2,',','.') }}</li>
+                <li><strong>Adicinado em: </strong>@if(isset($produto->created_at)) {{ date_format($produto->created_at, 'd/m/Y') }} @endif </li>
+            </ul>
+            <p>{{ $produto->descricao }}</p>
+        </div>
+    </div>
+
+    <a href="javascript:history.go(-1)">Voltar</a>
 @endsection
