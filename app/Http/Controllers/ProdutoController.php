7aa2ec05f22e219515283783b11586aafcfbec29
Tiago Santos <tiagorafaelmelo@gmail.com>
2019-04-16 21:41:45 -0300

    Atualizações.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -93,6 +93,12 @@ class ProdutoController extends Controller
             'preco'     =>  'required|numeric|',
         ], $msgm);
 
+        if ($request->hasFile('imgproduto')){
+            $imagem = $request->file('imgproduto');
+            $nomearquivo = md5($id).".".$imagem->getClientOriginalExtension();
+            $request->file('imgproduto')->move(public_path('./img/produtos/'), $nomearquivo);
+        }
+
         $produto->sku = $request->get('sku');
         $produto->titulo = $request->get('titulo');
         $produto->descricao = $request->get('descricao');
--- resources/views/produtos/edit.blade.php
+++ resources/views/produtos/edit.blade.php
@@ -5,11 +5,16 @@
 
     @include('layout.message-success-error')
 
-    <form method="POST" action="{{ route('produtos.update', $produto->id) }}">
+    <form method="POST" action="{{ route('produtos.update', $produto->id) }}" enctype="multipart/form-data">
         <input type="hidden" name="_method" value="PUT">
         {{ csrf_field() }}
 
         @include('produtos._form')
+
+        <div class="input-group mb-3">
+            <label for="imgproduto">Imagem</label>
+            <input class="form-control-file" type="file" id="imgproduto" name="imgproduto">
+        </div>
 
         <button type="submit" class="btn btn-primary">Atualizar Produto</button>
     </form>
