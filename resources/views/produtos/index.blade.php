b8ff21982a748afa3709714965a074a8b0ae0281
Tiago Rafael <tiagorafaelmelo@gmail.com>
2019-04-17 18:36:50 -0300

    Atualizacao

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -3,9 +3,17 @@
 @section('content')
     <h1>Produtos</h1>
 
-    <ul>
-    @foreach($produtos as $produto)
-        <li><a href="{{ route('produtos.show', $produto->id) }}">{{ $produto->titulo }}</a></li>
-    @endforeach
-    </ul>
+    <div class="row">
+        @foreach($produtos as $produto)
+            <div class="col-md-3">
+                @if(file_exists("./img/produtos/".md5($produto->id).".jpg"))
+                    <img src="{{ url('img/produtos/'.md5($produto->id).".jpg") }}" alt="Imagem Produto" class="img-fluid img-thumbnail">
+                @endif
+
+                <h4 class="text-center"><a href="#">{{ $produto->titulo }}</a></h4>
+                    <a class="btn btn-primary" href="{{ route('produtos.show', $produto->id) }}">Editar</a>
+                    <a class="btn btn-danger" href="">Excluir</a>
+            </div>
+        @endforeach
+    </div>
 @endsection
